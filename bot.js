1d37c1c39fe45c9db9db3952e68716175ca8243e
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2018-05-21 14:50:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -275,6 +275,31 @@ if(message.content.split(' ')[0] == '-bc') {
     
    });
 
+
+
+   client.on('message', message => {
+    if (message.content.startsWith("-bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle('Stats Bot / Info ')
+            .addField('``Uptime``', timeCon(process.uptime()), true)
+            .addField('``Ping Is``' , `${Date.now() - message.createdTimestamp}` + '``Ms``', true)
+            .addField('``RAM Usage``', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
+            .addField('``Guild Count``', client.guilds.size, true)
+            .addField('``Bot In channel``' , `${client.channels.size}` , true)
+            .addField('``Users rout``' ,`${client.users.size}` , true)
+            .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
+            .addField('``Bot Id``' , `${client.user.id}` , true)
+            .setFooter('Dragon')
+    })
+}
+});
+
+  
+
+
+
 client.on("message", message => {
  if (message.content === "-help") {
         message.react("✅")
@@ -332,26 +357,6 @@ client.on("message", message => {
    }); 
 
 
-   client.on('message', message => {
-    if (message.content.startsWith("-bot")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTitle('Stats Bot / Info ')
-            .addField('``Uptime``', timeCon(process.uptime()), true)
-            .addField('``Ping Is``' , `${Date.now() - message.createdTimestamp}` + '``Ms``', true)
-            .addField('``RAM Usage``', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
-            .addField('``Guild Count``', client.guilds.size, true)
-            .addField('``Bot In channel``' , `${client.channels.size}` , true)
-            .addField('``Users rout``' ,`${client.users.size}` , true)
-            .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
-            .addField('``Bot Id``' , `${client.user.id}` , true)
-            .setFooter('Dragon')
-    })
-}
-});
-
-  
 
 
 client.login(process.env.BOT_TOKEN);
