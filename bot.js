dcbb99554a244d06be99742f1d371c6a1bc222d2
legend-boker <39490532+legend-boker@users.noreply.github.com>
2018-06-22 04:15:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -350,7 +350,16 @@ if(message.content.split(' ')[0] == '-bc') {
 
 
 
-
+   client.on('message', message => {
+       if (message.content.startsWith(prefix + 'botserver')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#ebf442");
+  message.channel.send(embed);
+}
+});
 
   
    client.on('message', message => {
