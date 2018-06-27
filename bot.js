46fb89a6e0d3a576e7b951c621e035da940b9db0
jnfevsorvnm <40556229+jnfevsorvnm@users.noreply.github.com>
2018-06-27 18:29:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,12 @@ client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
 
+client.on('message', message => {
+if(message.content.startsWith(prefix + "invite")) { 
+message.author.send(` https://discordapp.com/oauth2/authorize?client_id=461155786492608513&permissions=8&scope=bot`);
+}
+});
+
 
 client.on('message', message => {
     if (message.content.startsWith("#hack")) {
