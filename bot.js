fef128a71411feb90f9d1adcf6e3c0177c626d1d
jnfevsorvnm <40556229+jnfevsorvnm@users.noreply.github.com>
2018-06-27 18:02:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,7 +137,23 @@ client.on("message",(message) => {
 
 
 
-
+client.on('message', message => {
+            if (message.content.startsWith("قوانين")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **اولا** ' ,' **ممنوع السب** ')
+.addField('     **ثانيا** ' ,' **لا تسوي سبام ** ')
+.addField('     **ثالثا** ' ,' **لا تزعج الاخرين** ')
+.addField('    **رابعا**' ,' **ممنوع الاعلانات** ')
+.addField('    **خامسا**' ,' **احترم الاخرين** ')
+.addField('    **سادسا**' ,' **لا تنشر في الشات او بل خاص** ')
+.addField('    **سابعا**' ,' **لا تنشر روابط!** ')
+.addField('    **ثامنا**' ,' **لا تسوي سبام ايموجي** ')
+.addField('    **تاسعا**' ,' **لا تطلب رتبه الاداره !** ')
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 
@@ -210,7 +226,6 @@ client.on("message",(message) => {
       })
       })
       });
-
 
 
 /*
