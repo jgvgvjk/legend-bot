40470a387c395c7e2cbafae65e630739ff2a6ae3
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-08 00:53:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,83 @@
-const Discord = require('discord.js');
+const figlet = require('figlet');
+const sql = require("sqlite");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const ytdl = require('ytdl-core');
-const request = require('request');
-const fs = require('fs');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
- 
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '-';
-const discord_token = process.env.BOT_TOKEN;
-client.login(discord_token);
-client.on('ready', function() {
-    console.log(`i am ready ${client.user.username}`);
+client.on('ready', () => {
+  client.user.setGame(`welcime-BOT | !help .`,'https://www.instagram.com/Abood_naimat0/');
+  console.log('-----------------')
+  console.log('welcome-Bot🌟 Is Online')
+  console.log('-----------------')
 });
 
+      client.on('guildMemberAdd', member => {
+      const welcomer = member.guild.channels.find('name', 'welcome');
+      if(!welcomer) return;
+         moment.locale('ar-ly');
+         var h = member.user;
+        let heroo = new Discord.RichEmbed()
+        .setColor('GREEN')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+        .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)
+        .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:heroo});
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+
+      const w = ['w1.png',
+      'w2.png',
+      'w3.png',
+      'w4.png',
+      'w5.png',
+      'w7.png',
+      'w8.png'];
+              let Image = Canvas.Image,
+                  canvas = new Canvas(401, 202),
+                  ctx = canvas.getContext('2d');
+              ctx.patternQuality = 'bilinear';
+              ctx.filter = 'bilinear';
+              ctx.antialias = 'subpixel';
+              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+              ctx.shadowOffsetY = 2;
+              ctx.shadowBlur = 2;
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 401, 202);
+
+      })
+
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+
+
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.drawImage(ava, 152, 27, 95, 95);
+
+                              ctx.font = '20px Georgia';
+                              ctx.fontSize = '5px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+                                                         ctx.fillText(member.user.username, 200, 154);
+                                    ctx.fillText(`member number${member.guild.memberCount} `
+                              , 200, 180);
+          
+
+ welcomer.sendFile(canvas.toBuffer())
+
+
+
+      })
+      })
+      });
 
 
 var dat = JSON.parse("{}");
